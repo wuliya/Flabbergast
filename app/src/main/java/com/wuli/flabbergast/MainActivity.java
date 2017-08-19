fbf64640883d696515f0865ae8f2b156fb6bb3be
liyawu <liya.j.wu@gmail.com>
2017-08-19 14:17:56 +0100

    Test display string

--- app/src/main/java/com/wuli/flabbergast/MainActivity.java
+++ app/src/main/java/com/wuli/flabbergast/MainActivity.java
@@ -2,6 +2,10 @@ package com.wuli.flabbergast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void refreshGrid(View view) {
+        List<String> grid = GridGenerator.generateGrid();
+        TextView gridAsText = (TextView) findViewById(R.id.gridAsText);
+        String displayString = "";
+        for (String letter : grid) {
+            displayString = displayString + letter;
+        }
+
+        gridAsText.setText(displayString);
     }
 }
